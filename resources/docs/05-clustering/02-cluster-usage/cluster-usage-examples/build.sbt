ee7cb0d3548b614f59a2420c7245b8324bb8b4be
Nico Serfontein <nserfontein@gmail.com>
2018-02-23 12:52:30 +0200

    Docs 2.5.9

--- resources/docs/05-clustering/02-cluster-usage/cluster-usage-examples/build.sbt
+++ resources/docs/05-clustering/02-cluster-usage/cluster-usage-examples/build.sbt
@@ -8,7 +8,10 @@ lazy val akkaVersion = "2.5.9"
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
-  "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+  "com.typesafe.akka" %% "akka-remote" % akkaVersion,
   "com.typesafe.akka" %% "akka-cluster" % akkaVersion,
+  "com.typesafe.akka" %% "akka-cluster-metrics" % akkaVersion,
+  "com.typesafe.akka" %% "akka-cluster-tools" % akkaVersion,
+  "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
   "org.scalatest" %% "scalatest" % "3.0.1" % "test"
 )
--- resources/docs/05-clustering/02-cluster-usage/cluster-usage-examples/src/main/scala/sample/cluster/simple/SimpleClusterListener.scala
+++ resources/docs/05-clustering/02-cluster-usage/cluster-usage-examples/src/main/scala/sample/cluster/simple/SimpleClusterListener.scala
@@ -1,8 +1,9 @@
-package scala.docs.cluster
+package sample.cluster.simple
 
-import akka.actor.{Actor, ActorLogging}
 import akka.cluster.Cluster
 import akka.cluster.ClusterEvent._
+import akka.actor.ActorLogging
+import akka.actor.Actor
 
 class SimpleClusterListener extends Actor with ActorLogging {
 
@@ -16,14 +17,13 @@ class SimpleClusterListener extends Actor with ActorLogging {
   override def postStop(): Unit = cluster.unsubscribe(self)
 
   def receive = {
-    case MemberUp(member) ⇒
+    case MemberUp(member) =>
       log.info("Member is Up: {}", member.address)
-    case UnreachableMember(member) ⇒
+    case UnreachableMember(member) =>
       log.info("Member detected as unreachable: {}", member)
-    case MemberRemoved(member, previousStatus) ⇒
-      log.info(
-        "Member is Removed: {} after {}",
+    case MemberRemoved(member, previousStatus) =>
+      log.info("Member is Removed: {} after {}",
         member.address, previousStatus)
-    case _: MemberEvent ⇒ // ignore
+    case _: MemberEvent => // ignore
   }
 }
